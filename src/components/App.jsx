59887cdb77133273f82b7c01f5cec7654a2bb096
BondAndrii <104005997+BondAndrii@users.noreply.github.com>
2023-04-20 15:52:46 +0300

    delete component Layout

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,49 @@
-import { Layout } from "./Layout/Layout";
+import React from "react";
 
-export const App = () => {
-  return (
-    <div
-      // style={{
-      //   height: '100vh',
-      //   display: 'flex',
-      //   justifyContent: 'center',
-      //   alignItems: 'center',
-      //   fontSize: 40,
-      //   color: '#010101'
-      // }}
-    >
-      <Layout/>
-      
-    </div>
-  );
-};
+import { Component } from "react";
+
+import { Buttons } from "components/Buttons/Buttons";
+
+import { Statistics } from "components/Statistics/Statistics";
+
+
+
+export class App extends Component {
+    state = {
+        Good: 0,
+        Neutral: 0,
+        Bad: 0,      
+    };
+    
+    handleClick = (evt) => {
+    const { name } = evt.target;
+    // console.log(name);
+    this.setState({ [name]: this.state[name] + 1 })
+    };
+
+    countTotalFeedback() {
+        const { Good, Neutral, Bad } = this.state;
+        const total = Good + Neutral + Bad;
+        console.log(total);
+        return total;        
+    }
+    countPositiveFeedbackPercentage() {
+        const { Good} = this.state;
+        const positive = (Good * 100) / this.countTotalFeedback();
+        return positive ? positive : 0 ;
+    }
+    render() {
+        
+       
+        return (
+            <>                              
+                <Buttons priClick={this.handleClick} ps={this.state} />                
+                <Statistics
+                    ps={this.state}
+                    total={this.countTotalFeedback()}
+                    positive={this.countPositiveFeedbackPercentage()}
+                    />
+            </>
+        )
+        }
+}
